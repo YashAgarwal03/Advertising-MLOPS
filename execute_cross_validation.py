8a65fa9f345c8d358a127396e2b4812826633463
YashAgarwal03 <datascientistyash03@gmail.com>
2024-12-22 21:57:34 +0530

    excute cross val pipeline complete

---
+++ execute_cross_validation.py
@@ -0,0 +1,23 @@
+from training.pipeline.common.data_ingestion import DataIngestionPipeline
+from training.pipeline.common.data_validation import DataValidationPipeline
+from training.pipeline.cross_val.cross_val import CrossValPipeline
+from training.custom_logging import info_logger
+
+PIPELINE = "Data Ingestion Training Pipeline"
+info_logger.info(f">>>>> {PIPELINE} started <<<<<")
+obj = DataIngestionPipeline()
+obj.main()
+info_logger.info(f">>>>> {PIPELINE} completed <<<<<")
+
+PIPELINE = "Data Validation Training Pipeline"
+info_logger.info(f">>>>>>>> {PIPELINE} sttarted <<<<<<<<<")
+obj = DataValidationPipeline()
+obj.main()
+info_logger.info(f">>>>>>>> {PIPELINE} completed <<<<<<<<<")
+
+PIPELINE = "Data Validation Pipeline"
+info_logger.info(f">>>>> {PIPELINE} started <<<<")
+obj = CrossValPipeline()
+obj.main()
+info_logger.info(f">>>>>>>> {PIPELINE} completed <<<<<<<<<")
+
