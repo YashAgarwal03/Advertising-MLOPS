41375ba4672907c5c2730b3694e9661ecc4b06fb
YashAgarwal03 <datascientistyash03@gmail.com>
2024-12-22 23:02:37 +0530

    predection pipeline complete

---
+++ deployment/pipeline/prediction_pipeline.py
@@ -0,0 +1,31 @@
+from deployment.custom_logging import info_logger, error_logger
+from deployment.exception import PredictionError, handle_exception
+
+from deployment.components.feature_engineering import FeatureEngineering
+from deployment.components.prediction import Prediction
+
+import os
+import sys
+from pathlib import Path
+import numpy as np
+class PredictionPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self, input):
+        try:
+            feature_engineering = FeatureEngineering()
+            transformed_data = feature_engineering.transform_data(input)
+
+            prediction = Prediction()
+            predicted_price = prediction.predict(transformed_data)
+
+            return predicted_price
+        except Exception as e:
+            handle_exception(e,PredictionError)
+
+if __name__ == "__main__":
+    prediction_pipeline = PredictionPipeline()
+    input = np.array([[112,34,54]])
+    prediction_pipeline = prediction_pipeline.predict(input)
+    print(prediction_pipeline)
